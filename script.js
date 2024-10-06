0a8c176c54708ba4b3ff348af7630fbaa9f54e95
asim1734 <asim.rupani@gmail.com>
2024-10-06 18:11:13 +0530

    Add function to paint the grid with a color

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ function generateGrid(){
 for(let i = 1 ; i < 16*16 + 1 ; i++){
     const box = document.createElement("div");
     box.classList.add("box");
-    box.textContent = i;
     container.appendChild(box);
 }
 }
 
+function addMouseListeners(){
+    let isMouseDown = false;
+    container.addEventListener("mousedown", () =>{
+        isMouseDown = true;
+    });
+
+    container.addEventListener("mouseup", () =>{
+        isMouseDown = false;
+    });
+
+    const children = Array.from(container.children);
+    children.forEach(child =>{
+        child.addEventListener("mouseover" ,function (event) {
+            if(isMouseDown){
+            event.target.style.backgroundColor = "red";
+            }
+        } )
+    })
+    
+}
+
 generateGrid();
+addMouseListeners();
 
